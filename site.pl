8ab2a054efc5908289c27e50905aba83fedbaa11
Thiago Delatorre <thiago.delatorre@gmail.com>
2015-10-12 23:18:27 -0400

    Show the calendar

--- site.pl
+++ site.pl
@@ -5,26 +5,192 @@ use warnings;
 use utf8;
  require LWP::UserAgent;
  
- our $ua = LWP::UserAgent->new;
- $ua->timeout(60);
+ our $ua = LWP::UserAgent->new(agent=>'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36');
+ push @{ $ua->requests_redirectable }, 'POST';
+ $ua->timeout(180);
  $ua->env_proxy;
  $ua->cookie_jar({file => "cookies.txt"});
 
-our $loginURL = 'https://prenotaonline.esteri.it/login.aspx?cidsede=100001&returnUrl=%2f';
+our $baseURL = 'https://prenotaonline.esteri.it/';
+our $loginURL = $baseURL.'login.aspx?cidsede=100001&returnUrl=%2f';
+our $username=''; our $password='';
  
-my $page = login();
-#my $page = getPage($loginURL);
-#my %formFields = getForm($page);
+unless($username=~/thiago/){
+	print "Username not set.\n";
+	exit(1);
+}
 
-print $page;
+my $page = login();
+#my $page = getPage("DocLeg.html");
+#$page = confirmDocLegal($page);
+
+#print $page;
 
 exit(0);
 
 sub login{
 	my $page = getPage($loginURL);
+	savePage($page);
+	print STDERR "Connected\n";
 	$page = changeLanguage($page);
+	savePage($page);
+	print STDERR "Language Changed\n";
 	$page = openLogin($page);
+	savePage($page);
+	print STDERR "Login Opened\n";
+	$page = clickLogin($page);
+	savePage($page);
+	print STDERR "Logged in\n";
+	$page = clickSchedule($page);
+	savePage($page);
+	print STDERR "Schedule clicked\n";
+	$page = clickDocLegal($page);
+	savePage($page);
+	print STDERR "Documents legalization clicked\n";
+	$page = confirmDocLegal($page);
+	savePage($page);
+	print STDERR "Documents legalization confirmed\n";
 	return $page;
+}
+
+sub confirmDocLegal{
+	my $page = $_[0];
+	
+	my %formFields = getForm($page);
+	delete $formFields{'ctl00$ContentPlaceHolder1$acc_datiAddizionali1$btnAnnulla'};
+	delete $formFields{'ctl00$repFunzioni$ctl00$btnMenuItem'};
+	delete $formFields{'ctl00$repFunzioni$ctl03$btnMenuItem'};
+	delete $formFields{'ctl00$repFunzioni$ctl02$btnMenuItem'};
+	delete $formFields{'ctl00$repFunzioni$ctl01$btnMenuItem'};
+	delete $formFields{'ctl00$btnLogout'};
+	
+	#foreach my $key (keys %formFields){
+	#	print $key, " = ", $formFields{$key},"\n";
+	#}
+	
+	$page = postPage($baseURL."acc_Prenota.aspx",\%formFields);
+
+	return $page;
+}
+
+sub clickDocLegal{
+	my $page = $_[0];
+
+	my %formFields = getForm($page);
+	delete $formFields{'ctl00$ContentPlaceHolder1$hiServizio'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$acc_breadcrumbServizi1$ctl01'};
+	delete $formFields{'ctl00$btnLogout'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl03$h_attivo'};
+	delete $formFields{'ctl00$repFunzioni$ctl03$btnMenuItem'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl01$h_idservizio'};
+	delete $formFields{'ctl00$repFunzioni$ctl02$btnMenuItem'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl01$h_settimane'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl02$h_idservizio'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl03$h_idservizio'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl03$h_bloccato'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl01$h_bloccato'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl02$h_settimane'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl01$btnNomeServizio'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl03$h_settimane'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl02$h_bloccato'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl01$h_attivo'};
+	delete $formFields{'ctl00$repFunzioni$ctl01$btnMenuItem'};
+	delete $formFields{'ctl00$repFunzioni$ctl00$btnMenuItem'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl02$h_attivo'};
+	delete $formFields{'ctl00$ContentPlaceHolder1$rpServizi$ctl02$btnNomeServizio'};
+	delete $formFields{'$ctl02$btnNomeServizio'};
+
+	#foreach my $key (keys %formFields){
+	#	print $key, " = ", $formFields{$key},"\n";
+	#}
+	
+	$page = postPage($baseURL."acc_Prenota.aspx",\%formFields);
+	
+	return $page;
+}
+
+sub clickSchedule{
+	my $page = $_[0];
+	my %formFields = getForm($page);
+	delete $formFields{'ctl00$repFunzioni$ctl02$btnMenuItem'};
+	delete $formFields{'ctl00$repFunzioni$ctl03$btnMenuItem'};
+	delete $formFields{'ctl00$repFunzioni$ctl01$btnMenuItem'};
+	delete $formFields{'ctl00$btnLogout'};
+
+	$page = postPage($baseURL."default.aspx",\%formFields);
+
+	return $page;
+}
+
+sub clickLogin{
+	my $page = $_[0];
+	
+	my %formFields = getForm($page);
+	delete $formFields{'repLanguages$ctl00$cmdLingua'};
+	delete $formFields{'repLanguages$ctl01$cmdLingua'};
+	delete $formFields{'BtnRegistrati'};
+	delete $formFields{'BtnLogin'};
+	delete $formFields{'BtnPwdDimenticataL'};
+	$formFields{"UserName"}=$username;
+	$formFields{"Password"}=$password;
+
+	my $captcha = solveCaptcha($page);
+	$formFields{"loginCaptcha"} = $captcha;
+
+	#foreach my $key (keys %formFields){
+	#	print $key, " = ", $formFields{$key},"\n";
+	#}
+
+	$page = postPage($loginURL, \%formFields);
+	
+	return $page;
+}
+
+sub solveCaptcha{
+	my $page = $_[0];
+	my $captcha = "";
+
+	my $url = getCaptchaURL($page);
+	
+	my $captchaFile = fileFromURL($url);
+
+	print "Download captcha from here: $url\n";
+
+	if(-e "Captcha/".$captchaFile){
+		#print "Known captcha.\n";
+		open FILE, "< Captcha/$captchaFile";
+		$captcha = <FILE>;
+		chomp $captcha;
+		close FILE;
+	} else {
+		#print "Unknown captcha.\n";
+		getCaptchaImage($url);
+		system("display \"Cache/$captchaFile\" &");
+		print "Enter the solution for the captcha: ";
+		$captcha = <STDIN>;
+		chomp $captcha;
+		open FILE, "> Captcha/$captchaFile";
+		print FILE $captcha;
+		close FILE;
+	}
+
+	return $captcha;
+}
+
+sub getCaptchaURL{
+	my $page = $_[0];
+	my $url = "";
+
+	if($page=~/<img id="captchaLogin"[^>]+src="([^"]+)"/){
+		$url = $baseURL.$1;
+	}
+
+	return $url;
+}
+
+sub getCaptchaImage{
+	my $url = $_[0];
+	my $image = getPage($url);
 }
 
 sub openLogin{
@@ -37,15 +203,16 @@ sub openLogin{
 	delete $formFields{'BtnRegistrati'};
 
 
-	foreach my $key (keys %formFields){
-		print $key, " = ", $formFields{$key},"\n";
-	}
+	#foreach my $key (keys %formFields){
+	#	print $key, " = ", $formFields{$key},"\n";
+	#}
 	
-	print "Something wrong!\n";
+	#print "Something wrong!\n";
 	$page = postPage($loginURL, \%formFields);
 
 	return $page;
 }
+
 sub changeLanguage{
 	my $page = $_[0];
 
@@ -134,10 +301,32 @@ sub getForm{
 		$originalPage=~s/<input[^<]*name="([^"]+)"[^<]*value="([^"]+)"//;
 		$formFields{$name}=$value;
 	}
-	
-	foreach my $key (keys %formFields){
-		print $key, " = ", $formFields{$key}, "\n";
+	while($originalPage=~/<input[^<]*name="([^"]+)"/){
+		my ($name,$value)=($1,"");
+		$originalPage=~s/<input[^<]*name="([^"]+)"//;
+		$formFields{$name}=$value;
 	}
+	
+	#foreach my $key (keys %formFields){
+	#	print $key, " = ", $formFields{$key}, "\n";
+	#}
 
 	return %formFields;
+}
+
+sub savePage{
+	my $page = $_[0];
+
+	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+                                                localtime(time);
+
+	my $filename = "Log/$year$mon$mday$hour$min$sec.html";
+	#print $filename,"\n";
+	
+	open FILE, "> $filename" || die $!;
+
+	print FILE $page;
+
+	close FILE;
+	sleep(1);
 }
